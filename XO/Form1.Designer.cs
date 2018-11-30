cb6806cb1ebf3087c32d12008e39cf4d11ce6b94
Elvin Mammadov <36962425+themammadov@users.noreply.github.com>
2018-11-30 16:39:34 +0400

    Tic Tac Toe

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.yolchuScore = new System.Windows.Forms.Label();
+            this.elvinScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yolchuTurn = new System.Windows.Forms.Label();
+            this.elvinTurn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(118, 69);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(80, 75);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(204, 69);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(80, 75);
+            this.btn2.TabIndex = 0;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(290, 69);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(80, 75);
+            this.btn3.TabIndex = 0;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn5.FlatAppearance.BorderSize = 0;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(204, 150);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(80, 75);
+            this.btn5.TabIndex = 0;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(118, 150);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(80, 75);
+            this.btn4.TabIndex = 0;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn6.FlatAppearance.BorderSize = 0;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(290, 150);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(80, 75);
+            this.btn6.TabIndex = 0;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn7.FlatAppearance.BorderSize = 0;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(118, 231);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(80, 75);
+            this.btn7.TabIndex = 0;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn9.FlatAppearance.BorderSize = 0;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(290, 231);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(80, 75);
+            this.btn9.TabIndex = 0;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn8.FlatAppearance.BorderSize = 0;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(204, 231);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(80, 75);
+            this.btn8.TabIndex = 0;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.AllBtnsClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Sienna;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Təmizlə";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Sienna;
+            this.button2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(248, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Yenidən başla";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.BackColor = System.Drawing.Color.LimeGreen;
+            this.lblTurn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTurn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTurn.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.Location = new System.Drawing.Point(504, 69);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(71, 33);
+            this.lblTurn.TabIndex = 2;
+            this.lblTurn.Text = "Sıra :";
+            // 
+            // yolchuScore
+            // 
+            this.yolchuScore.AutoSize = true;
+            this.yolchuScore.BackColor = System.Drawing.Color.Aqua;
+            this.yolchuScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.yolchuScore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yolchuScore.Font = new System.Drawing.Font("Microsoft YaHei", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yolchuScore.Location = new System.Drawing.Point(504, 231);
+            this.yolchuScore.Name = "yolchuScore";
+            this.yolchuScore.Size = new System.Drawing.Size(2, 33);
+            this.yolchuScore.TabIndex = 2;
+            // 
+            // elvinScore
+            // 
+            this.elvinScore.AutoSize = true;
+            this.elvinScore.BackColor = System.Drawing.Color.Aqua;
+            this.elvinScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.elvinScore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.elvinScore.Font = new System.Drawing.Font("Microsoft YaHei", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elvinScore.Location = new System.Drawing.Point(583, 231);
+            this.elvinScore.Name = "elvinScore";
+            this.elvinScore.Size = new System.Drawing.Size(2, 33);
+            this.elvinScore.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yolchu vs Elvin";
+            // 
+            // yolchuTurn
+            // 
+            this.yolchuTurn.AutoSize = true;
+            this.yolchuTurn.BackColor = System.Drawing.Color.LimeGreen;
+            this.yolchuTurn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.yolchuTurn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yolchuTurn.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yolchuTurn.Location = new System.Drawing.Point(435, 129);
+            this.yolchuTurn.Name = "yolchuTurn";
+            this.yolchuTurn.Size = new System.Drawing.Size(2, 33);
+            this.yolchuTurn.TabIndex = 2;
+            // 
+            // elvinTurn
+            // 
+            this.elvinTurn.AutoSize = true;
+            this.elvinTurn.BackColor = System.Drawing.Color.LimeGreen;
+            this.elvinTurn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.elvinTurn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.elvinTurn.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elvinTurn.Location = new System.Drawing.Point(566, 129);
+            this.elvinTurn.Name = "elvinTurn";
+            this.elvinTurn.Size = new System.Drawing.Size(2, 33);
+            this.elvinTurn.TabIndex = 2;
+            this.elvinTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 450);
+            this.Controls.Add(this.elvinScore);
+            this.Controls.Add(this.yolchuScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.elvinTurn);
+            this.Controls.Add(this.yolchuTurn);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        public System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label yolchuScore;
+        private System.Windows.Forms.Label elvinScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label yolchuTurn;
+        private System.Windows.Forms.Label elvinTurn;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        Button[,] buttons = new Button[3, 3];
+
+        private bool XorNot;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new Button[3, 3]
+            {
+                { btn1, btn2, btn3 }, { btn4, btn5, btn6 }, { btn7, btn8, btn9
+            }};
+
+
+
+        }
+
+        private void AllBtnsClick(object sender, EventArgs e)
+        {
+            yolchuScore.Visible = false;
+            elvinScore.Visible = false;
+            Button btn = sender as Button;
+            btn.BackColor = Color.Aqua;
+            if (XorNot == false)
+            {
+
+                btn.Text = "X";
+                XorNot = true;
+                btn.Enabled = false;
+                yolchuTurn.Text = "Yolchu";
+                elvinTurn.Hide();
+                yolchuTurn.Show();
+                
+                if (!Check())
+                {
+                    SummaryCheck();
+                    
+
+                }
+               
+
+
+
+            }
+            
+            else
+            {
+
+                btn.Text = "O";
+                XorNot = false;
+                btn.Enabled = false;
+                elvinTurn.Text = "Elvin";
+                elvinTurn.Show();
+                yolchuTurn.Hide();
+                
+                if (!Check())
+                {
+                    SummaryCheck();
+                   
+
+                }
+              
+
+            }
+           
+        }
+
+        private bool Check()
+        {
+            yolchuScore.Visible = false;
+            elvinScore.Visible = false;
+            int rown = 0;
+            bool x = false;
+            for (int row = 0; row < buttons.GetLength(0); row++)
+            {
+                int col = 0;
+
+                if (!String.IsNullOrEmpty(buttons[row, col].Text) && !String.IsNullOrEmpty(buttons[row, col + 1].Text) && !String.IsNullOrEmpty(buttons[row, col + 2].Text))
+                {
+                    if ((buttons[row, col].Text == buttons[row, col + 1].Text) && (buttons[row, col].Text == buttons[row, col + 2].Text))
+                    {
+                       
+                        x = true;
+                        ButtonEnable();
+                        elvinScore.Visible = true;
+                        yolchuScore.Visible = true;
+                        elvinScore.Text = "1";
+                        yolchuScore.Text = "0";
+                        MessageBox.Show("You are winner");
+                        return x;
+                       
+
+                    }
+
+
+                }
+
+
+            }
+
+
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                if (!String.IsNullOrEmpty(buttons[rown, i].Text) && !String.IsNullOrEmpty(buttons[rown + 1, i].Text) && !String.IsNullOrEmpty(buttons[rown + 2, i].Text))
+                {
+                    if (buttons[rown, i].Text == buttons[rown + 1, i].Text && buttons[rown, i].Text == buttons[rown + 2, i].Text)
+                    {
+                        
+                        x = true;
+                        ButtonEnable();
+                        elvinScore.Visible = true;
+                        yolchuScore.Visible = true;
+                        yolchuScore.Text = "0";
+                        elvinScore.Text = "1";
+                        MessageBox.Show("You are winner");
+                        return x;
+
+
+                    }
+
+                }
+
+
+            }
+
+
+            if (!String.IsNullOrEmpty(buttons[rown, rown].Text) && !String.IsNullOrEmpty(buttons[rown + 1, rown + 1].Text) && !String.IsNullOrEmpty(buttons[rown + 2, rown + 2].Text))
+            {
+
+                if (buttons[rown, rown].Text == buttons[rown + 1, rown + 1].Text && buttons[rown, rown].Text == buttons[rown + 2, rown + 2].Text)
+                {
+                   
+                    x = true;
+                    ButtonEnable();
+                    MessageBox.Show("You are winner");
+                    return x;
+                }
+
+            }
+
+
+            if (!String.IsNullOrEmpty(buttons[rown, rown + 2].Text) && !String.IsNullOrEmpty(buttons[rown + 1, rown + 1].Text) && !String.IsNullOrEmpty(buttons[rown + 2, rown].Text))
+            {
+                if (buttons[rown, rown + 2].Text == buttons[rown + 1, rown + 1].Text && buttons[rown, rown + 2].Text == buttons[rown + 2, rown].Text)
+                {
+                    
+                    x = true;
+                    ButtonEnable();
+                    MessageBox.Show("You are winner");
+                    return x;
+
+
+                }
+
+
+            }
+            return x;
+
+
+        }
+        private void SummaryCheck()
+        {
+            bool x = true;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttons.GetLength(0); j++)
+                {
+                    if (String.IsNullOrEmpty(buttons[i, j].Text))
+                    {
+                        x = false;
+                        break;
+                    }
+                }
+            }
+            if (x)
+            {
+                DialogResult dialogResult = MessageBox.Show("Yenidən başlamaq istəyirsinizmi ?", " Qalib müəyyən olunmadı !", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Form1 form = new Form1();
+                    this.Hide();
+                    form.ShowDialog();
+                }
+               
+            }
+        }
+
+
+        private void ButtonEnable()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //When clear button is clicked , "btn"-s backcolor will be dark orange
+            btn1.BackColor = Color.DarkOrange;
+            btn2.BackColor = Color.DarkOrange;
+            btn3.BackColor = Color.DarkOrange;
+            btn4.BackColor = Color.DarkOrange;
+            btn5.BackColor = Color.DarkOrange;
+            btn6.BackColor = Color.DarkOrange;
+            btn7.BackColor = Color.DarkOrange;
+            btn8.BackColor = Color.DarkOrange;
+            btn9.BackColor = Color.DarkOrange;
+
+            //When clear button is clicked, "btn"-s text will be empty
+
+            btn1.Text = string.Empty;
+            btn2.Text = string.Empty;
+            btn3.Text = string.Empty;
+            btn4.Text = string.Empty;
+            btn5.Text = string.Empty;
+            btn6.Text = string.Empty;
+            btn7.Text = string.Empty;
+            btn8.Text = string.Empty;
+            btn9.Text = string.Empty;
+
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            this.Hide();
+            form.ShowDialog();
+        }
+    }
+}
+
